d43cc63c25b4e9b76438c84970f37edd7e28e5d6
liziwl <leezisy@gmail.com>
2021-06-25 19:02:23 +0800

    update test

--- test/test.sh
+++ test/test.sh
@@ -2,20 +2,20 @@
 set -x
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-mkdir "$SCRIPT_DIR/../public-test"
+RT_DIR="$( dirname "${SCRIPT_DIR}" )"
 
 # 备份原文件
-cp "$SCRIPT_DIR/../sustechthesis-example.tex" "$SCRIPT_DIR/../sustechthesis-example.tex.bak"
+cp "$RT_DIR/sustechthesis-example.tex" "$RT_DIR/sustechthesis-example.tex.bak"
 
 # 生成中文预览
-cp "$SCRIPT_DIR/sustechthesis-example-cn.tex" "$SCRIPT_DIR/../sustechthesis-example.tex"
+cp "$SCRIPT_DIR/sustechthesis-example-cn.tex" "$RT_DIR/sustechthesis-example.tex"
 make
-cp "$SCRIPT_DIR/../sustechthesis-example.pdf" "$SCRIPT_DIR/../public-test/sustechthesis-example-cn.pdf"
+cp "$RT_DIR/sustechthesis-example.pdf" "$RT_DIR/public-test/sustechthesis-example-cn.pdf"
 
 # 生成英文预览
-cp "$SCRIPT_DIR/sustechthesis-example-en.tex" "$SCRIPT_DIR/../sustechthesis-example.tex"
+cp "$SCRIPT_DIR/sustechthesis-example-en.tex" "$RT_DIR/sustechthesis-example.tex"
 make
-cp "$SCRIPT_DIR/../sustechthesis-example.pdf" "$SCRIPT_DIR/../public-test/sustechthesis-example-en.pdf"
+cp "$RT_DIR/sustechthesis-example.pdf" "$RT_DIR/public-test/sustechthesis-example-en.pdf"
 
 # 恢复源文件
-mv "$SCRIPT_DIR/../sustechthesis-example.tex.bak" "$SCRIPT_DIR/../sustechthesis-example.tex"
+mv "$RT_DIR/sustechthesis-example.tex.bak" "$RT_DIR/sustechthesis-example.tex"
